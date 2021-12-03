8e2f8941ef606fb790925e16ba3a90b5010581fd
fddiashok <fddiashok@gmail.com>
2021-12-02 21:02:42 -0500

    implemented call back inside request method

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,23 +4,33 @@ const array = process.argv.slice(2);
 // Extracting first three characters from breed name
 // const breed = array[0].slice(0,3).toLowerCase();
 const breed = array[0];
-console.log(breed);
+// console.log(breed);
 
-request(`https://api.thecatapi.com/v1/breeds/seach?q=${breed}`, (error, response, body) => {
+const fetchBreedDescription = function (breedName, callback) {
 
-  if (error) {
-    // Display an error if any
-    console.log("Error:", error);
-    return;
-  }
-  // // Check response code
-  // console.log("Response :", response&& response.statusCode);
+  console.log("outside request");
 
-  //Convert response string into object using JSON.parse()
-  const data = JSON.parse(body);
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
 
-  // console.log(typeof data);
-  // console.log("Body :", data);
-  if (data.length === 0)
-    console.log("Breed Not Found");
-});
+    console.log("inside request ");
+    // // Check response code
+    // console.log("Response :", response&& response.statusCode);
+
+    //Convert response string into object using JSON.parse()
+    const data = JSON.parse(body);
+
+    // console.log(typeof data);
+    // console.log("Body :", data);
+    if (data.length === 0)
+      return callback(null, "Breed Not Found");
+    else
+      return callback(null, data[0].description);
+  });
+};
+
+const printDescription = (error, toPrint) => {
+  console.log(toPrint);
+}
+
+fetchBreedDescription(breed, printDescription);
+console.log("in the end");
